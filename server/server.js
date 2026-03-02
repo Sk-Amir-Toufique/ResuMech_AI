c0dd83e344fed2a56240a4adf69ed8df56b44786
Sk Amir Toufique <toufiquesk13@gmail.com>
2026-03-02 15:00:12 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+import resumeRouter from "./routes/resumeRoutes.js";
+import aiRouter from "./routes/aiRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// database connection
+await connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Server is running..."));
+app.use("/api/users", userRouter);
+app.use("/api/resumes", resumeRouter);
+app.use("/api/ai", aiRouter);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
